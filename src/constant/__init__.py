5cbfdb0e3a47b4673758b4f2a67559faf42f3997
Adityagit <kdshuklaknpav3@gmail.com>
2026-01-15 11:03:52 +0000

    executed peoject

---
+++ src/constant/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "adya"
+MONGO_COLLECTION_NAME = "wafer_fault"
+
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://aditya4321:Ars%404950@cluster0.mevuthz.mongodb.net/?appName=Cluster0"
+
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
